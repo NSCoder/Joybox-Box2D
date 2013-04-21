f6e8ad36dd8df8051af734ee56b0e038a103c144
gzfrancisco <gz.francisco@gmail.com>
2013-04-20 21:53:42 -0500

    Added CircleShape initMethod

---
+++ Box2D/Collision/Shapes/B2DCircleShape.h
@@ -0,0 +1,16 @@
+//
+//  B2DCircleShape.h
+//  Box2D
+//
+//  Created by Francisco Granados on 20/04/13.
+//  Copyright (c) 2013 CurveBeryl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "B2DShape.h"
+
+@interface B2DCircleShape : B2DShape
+
+- (id)initWithPosition:(CGPoint)point;
+
+@end
